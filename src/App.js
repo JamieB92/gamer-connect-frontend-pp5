dd8aed81b4d3c610c4de7d5a665b7206703fc028
JamieB92 <j.bonner6992@gmail.com>
2023-11-28 21:57:28 +0000

    Create and style form for signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import { Route,Switch } from 'react-router-dom'
 import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 
 
@@ -14,7 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() =><SignUpForm/>} />
         </Switch>
       </Container>
     </div>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+const SignUpForm = () => {
+  return (
+    <Row className={styles.Row}>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={
+            "https://res.cloudinary.com/dxxzc99pz/image/upload/v1701207910/pexels-pixabay-159393_j8ns1h.jpg"
+          }
+        />
+      </Col>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign up</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+            </Form.Group>
+
+            <Button className={`${btnStyles.btn}`} type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;

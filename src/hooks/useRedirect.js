29e1c644754e0335abc241cc1e2e71a507b3a9e1
JamieB92 <j.bonner6992@gmail.com>
2023-12-12 20:54:29 +0000

    fix useRecdirect

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,25 +1,26 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import { useHistory } from "react-router";
 
 export const useRedirect = (userAuthStatus) => {
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                await axios.posts('/dj-rest-auth/token/refresh/')
-                // If user is logged in this will run
-                if (userAuthStatus === 'loggedIn'){
-                    history.push('/')
-                }
-            } catch(err) {
-                // if user is not logged in this will run
-                if (userAuthStatus === 'loggedOut'){
-                    history.push('/')
-                }
-            }
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        await axios.post("/dj-rest-auth/token/refresh/");
+        // If user is logged in this will run
+        if (userAuthStatus === "loggedIn") {
+          history.push("/");
         }
-        handleMount();
-    }, [history, userAuthStatus])
-}
+      } catch (err) {
+        // if user is not logged in this will run
+        if (userAuthStatus === "loggedOut") {
+          history.push("/");
+        }
+      }
+    };
+
+    handleMount();
+  }, [history, userAuthStatus]);
+};

5d4ef0477fc827e0dce41f17fdc81cf9b8baebf0
JamieB92 <j.bonner6992@gmail.com>
2023-12-12 10:49:16 +0000

    create the Gamespage, this page displays all users current games being played

---
+++ src/pages/games/GamesPage.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { useLocation } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Game from "./Game";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.jpg";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+
+
+
+const GamesPage = ({ message, filter = "" }) => {
+  const [games, setGames] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  const currentUser = useCurrentUser();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const { data } = await axiosReq.get(`/games/?${filter}search=${query}`);
+        setGames(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchGames();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname, currentUser]);
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <PopularProfiles mobile />
+        </Col>
+      </Row>
+      <Row className="h-100">
+        <Col>
+          <form
+            className={styles.searchForm}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <input
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              className={styles.searchBar}
+              placeholder="Search games bing played"
+            />
+          </form>
+        </Col>
+      </Row>
+      
+      <Row className="h-100">
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+            <PopularProfiles />
+        </Col>
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+          {hasLoaded ? (
+            <>
+              {games.results.length ? (
+                <InfiniteScroll 
+                  children={
+                    games.results.map((game) => (
+                      <Game key={game.id} {...game} setPosts={setGames} />
+                    ))
+                  }
+                  dataLength={games.results.length}
+                  loader={< Asset spinner />}
+                  hasMore={!!posts.next}
+                  next={() => fetchMoreData(games, setGamess)}
+                />
+              ) : (
+                <Container>
+                  <img
+                    src={NoResults}
+                    alt="No Results to display"
+                    className={styles.noResultsImage}
+                  />
+                  <Asset message={message}></Asset>
+                </Container>
+              )}
+            </>
+          ) : (
+            <Container className={appStyles.Content}>
+              <Asset spinner />
+            </Container>
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default GamesPage;
--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
+import Game from "../games/Game";
 import Asset from "../../components/Assests";
 import PostStyles from "../../styles/PostsPage.module.css";
 import styles from "../../styles/ProfilePage.module.css";
@@ -12,55 +12,80 @@ import { ProfileEditDropdown } from "../../components/EditDeleteDropdown";
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import {
   useProfileData,
   useSetProfileData,
 } from "../../contexts/ProfileDataContext";
-import { Image } from "react-bootstrap";
+import { Button, Image, Modal } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../../posts/Post";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.jpg";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import axios from "axios";
 
-function ProfilePage() {
+const ProfilePage = () => {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profilePosts, setProfilePosts] = useState({ results: [] });
+  const [gameData, setGameData] = useState(null);
 
   const currentUser = useCurrentUser();
   const { id } = useParams();
 
   const { setProfileData, handleFollow, handleUnfollow } = useSetProfileData();
   const { pageProfile } = useProfileData();
-
   const [profile] = pageProfile.results;
-  const is_owner = currentUser?.username === profile?.owner;
 
+  const is_owner = currentUser?.username === profile?.owner;
+  const gameId = profile?.gameId;
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDeleteGame = async () => {
+    try {
+      await axios.delete(`/games/${gameId}/`);
+      await axiosRes.put(`/profiles/${id}/`, { gameId: null });
+      setGameData(null);
+    } catch (err) {}
+    handleClose();
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
+    const handleMount = async () => {
       try {
         const [{ data: pageProfile }, { data: profilePosts }] =
           await Promise.all([
             axiosReq.get(`/profiles/${id}/`),
-            axiosReq.get(`/posts/?owner__profile=${id}`),
+            axiosReq.get(`/posts/?owner__profile=${id}`)
           ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
         }));
         setProfilePosts(profilePosts);
+        try {
+          const { data } = await axiosReq.get(`/games/${gameId}/`);
+          setGameData(data);
+        } catch (err) {
+          setGameData(null);
+        }
         setHasLoaded(true);
       } catch (err) {
-        console.log(err);
+        setGameData(null);
       }
     };
-    fetchData();
-  }, [id, setProfileData]);
+    handleMount();
+  }, [id, setProfileData, gameId]);
 
   const mainProfile = (
     <div className={styles.ProfileContent}>
-      {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
+      {profile?.is_owner && <ProfileEditDropdown 
+      id={profile?.id} 
+      handleDeleteGame={handleDeleteGame}
+      />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
@@ -75,8 +100,9 @@ function ProfilePage() {
             {profile?.platform && <h5 className={styles.name}>{profile.name}</h5>}
           </div>
           <div>
+          {profile?.gameId && <p>Looking for friends to play with on:</p>}
           {profile?.platform && <p>Prefered Platform: {profile.platform}</p>}
-          {profile?.platform_username && <p>Username: {profile.platform_username}</p>}
+          {profile?.platform_username && <p>My Username is: {profile.platform_username}</p>}
           </div>
         
           <Row className="justify-content-center no-gutters">
@@ -154,10 +180,48 @@ function ProfilePage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <Container>
           {hasLoaded ? (
-            <>
-              {mainProfile}
-              {mainProfilePosts}
-            </>
+            <div>
+              <div>
+                {mainProfile}
+              </div>
+              <div>
+              {profile?.is_owner && <Link 
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/games/create"> Tell everyone what you are playing currently!</Link>}
+
+              {profile?.gameId && is_owner && (
+                <button className={btnStyles.btnXsSmall} onClick={handleShow}>
+                  Remove Game data
+                </button>
+              )}
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Confirm Deletion</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  Are you sure you want to delete your current game data?
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <button
+                    className={btnStyles.btnXsSmall}
+                    onClick={handleDeleteGame}
+                  >
+                    Confirm
+                  </button>
+                </Modal.Footer>
+              </Modal>
+              {gameData && (
+                <Game {...gameData} isProfilePage showAll />
+              )}
+              </div>
+              <div>
+                {mainProfilePosts}
+              </div>
+            </div>
           ) : (
             <Asset spinner />
           )}
